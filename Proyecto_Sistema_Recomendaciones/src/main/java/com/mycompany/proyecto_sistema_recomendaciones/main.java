a17145c7d77fbe6fe1099b64d4d39e255e954276
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-05-08 11:35:27 -0600

    Creacion main

---
+++ Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/main.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_sistema_recomendaciones;
+
+
+public class main {
+    
+}
