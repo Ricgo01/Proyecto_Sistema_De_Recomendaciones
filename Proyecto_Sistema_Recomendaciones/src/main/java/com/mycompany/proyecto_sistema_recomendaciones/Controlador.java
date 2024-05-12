465ada3169a6210581ba95af24a022f79dae37c5
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-05-11 23:29:29 -0600

    Creacion de clase controlador

---
+++ Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/Controlador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_sistema_recomendaciones;
+
+import java.util.List;
+
+public class Controlador {
+    
+    
+    
+    //Este metodo es el que gracias a Cypher se recorrera un hash map con las caracteristicas que el usuario desea del perro y se pondra un puntaje de acuerdo al nivel de coincidencia, esto para retornar a los perros recomendados
+    public List<String> PerrosRecomendados(){
+        return null;
+    }
+    
+}
