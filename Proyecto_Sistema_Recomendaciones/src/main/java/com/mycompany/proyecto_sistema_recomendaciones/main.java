c32bd023d1b309e995404000ad2f54773015262c
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-05-14 16:50:58 -0600

    Codigo Cypher para buscar las relaciones entre los perros y asignar puntiaciones para que salgan los perros que mejor hacen match

--- Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/main.java
+++ Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/main.java
@@ -3,35 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.proyecto_sistema_recomendaciones;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Session;
-import org.neo4j.driver.Result;
 
 public class main {
     public static void main(String[] args) {
-        String uri = "neo4j+s://0503f9ee.databases.neo4j.io";
-        String user = "neo4j";
-        String password = "EPzLf4ZMyFJYAB-dr4bDr1rQ6B1M3aSpnpB6d9qHTPA";
-        
-        // Crear el driver y abrir una sesión
-        try (Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
-             Session session = driver.session()) {
-             
-            // Consulta para obtener solo los nombres de los perros
-            String query = "MATCH (d:Perro) RETURN d.nombre AS nombre";
-            Result result = session.run(query);
-            
-            // Imprimir los nombres de los perros
-            System.out.println("Nombres de los perros en la base de datos:");
-            while (result.hasNext()) {
-                var record = result.next();
-                System.out.println(record.get("nombre").asString());
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Controlador controlador = new Controlador();
+        controlador.recomendarPerros("Negro", "Corto", "Tranquilo/Relajado", "Grande", "Frios");
+        controlador.close();
     }
 }
