749a1a327d8e0dca8150e634c0e8360554afd1c3
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-05-14 21:35:38 -0600

    Conection

--- Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/Neo4jConnection.java
+++ Proyecto_Sistema_Recomendaciones/src/main/java/com/mycompany/proyecto_sistema_recomendaciones/Neo4jConnection.java
@@ -4,51 +4,23 @@
  */
 package com.mycompany.proyecto_sistema_recomendaciones;
 
-
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
-import org.neo4j.driver.types.Node;
-import org.neo4j.driver.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Neo4jConnection {
-    
-   public static void main(String[] args) {
-        
-    String uri = "neo4j+s://0503f9ee.databases.neo4j.io"; // O la dirección IP de tu servidor Neo4j
-    String user = "neo4j";
-    String password = "EPzLf4ZMyFJYAB-dr4bDr1rQ6B1M3aSpnpB6d9qHTPA";
+    private Driver driver;
 
-    // Crear el Driver y la Sesión
-    try (Driver driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
-         Session session = driver.session()) {
+    public Neo4jConnection(String uri, String user, String password) {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
 
-        // Consulta Cypher para recuperar todos los nodos y relaciones
-        String query = "MATCH (n) RETURN n";
-
-        // Ejecutar la consulta y procesar los resultados
-        Result result = session.run(query);
-        while (result.hasNext()) {
-            org.neo4j.driver.Record record = result.next();
-            // Procesar cada nodo y sus propiedades
-            Node node = record.get("n").asNode();
-            System.out.println("Node ID: " + node.id() + ", Labels: " + node.labels());
-            node.keys().forEach(key -> System.out.println(key + ": " + node.get(key)));
-            System.out.println();
-        }
-
-    } catch (Exception e) {
-        // Manejar la excepción
-        e.printStackTrace();
+    public Session createSession() {
+        return driver.session();
+    }
+//conection
+    public void close() {
+        driver.close();
     }
 }
-   
-
-}
-    
